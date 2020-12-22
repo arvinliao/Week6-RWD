fa70d57bd069a3119a8c5adae4b9adc107cc447b
arvinliao <LKS800507@gmail.com>
2020-12-22 14:51:12 +0800

    Update 2020-12-22T06:51:01.578Z

---
+++ assets/js/all.js
@@ -0,0 +1,17 @@
+"use strict";
+
+console.log('Hello!');
+$(document).ready(function () {
+  console.log('HesSchool Hello!');
+});
+"use strict";
+
+AOS.init({
+  offset: 200,
+  // offset (in px) from the original trigger point
+  delay: 0,
+  // values from 0 to 3000, with step 50ms
+  duration: 800 // values from 0 to 3000, with step 50ms
+
+});
+//# sourceMappingURL=all.js.map
